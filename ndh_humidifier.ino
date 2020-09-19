2295ec938f4a52da9cfd86084b48f9353c5328f7
Nathan Douglas <ndouglas@ainonline.com>
2020-09-19 14:40:27 -0500

    Adds OTA updating, an asynchronous webserver, and read/write lower and upper bounds that are stored in emulated EEPROM.

--- ndh_humidifier.ino
+++ ndh_humidifier.ino
@@ -1,7 +1,15 @@
 #include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP_EEPROM.h>
 #include <DHT.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-// Includes WIFI_SSID and WIFI_PASS.
+// Includes:
+//  - WIFI_SSID
+//  - WIFI_PASS
+//  - OTA_PASSWORD
 #include "secrets.h";
 
 // DHT22
@@ -16,14 +24,37 @@ unsigned long lastSampled = millis();
 // The pin that rocks the relay...
 // is the pin that rules the world.
 #define RELAY_PIN D6
-#define RELAY_LOWER_BOUND 75.0
-#define RELAY_UPPER_BOUND 90.0
+
+// Web Server
+AsyncWebServer webServer(80);
+
+// EEPROM
+struct eepromStruct {
+  float humidityLowerBound; // 4 bytes
+  float humidityUpperBound; // 4 bytes
+  int unused0; // 4 bytes
+  int unused1; // 4 bytes
+} settings;
 
 void setup() {
 
   // Serial.
   Serial.begin(115200);
   Serial.println();
+
+  // EEPROM emulation.
+  EEPROM.begin(sizeof(eepromStruct));
+
+  // Settings.
+  settings.humidityLowerBound = 75.0;
+  settings.humidityUpperBound = 90.0;
+  settings.unused0 = 0;
+  settings.unused1 = 1;
+
+  // Read from EEPROM if values have been set there.
+  if (EEPROM.percentUsed() >= 0) {
+    EEPROM.get(0, settings);
+  }
 
   // Relay Pin.
   pinMode(RELAY_PIN, OUTPUT);
@@ -42,6 +73,77 @@ void setup() {
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
 
+  // HTTP GET /temperature
+  webServer.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(temperature, 3));
+  });
+
+  // HTTP GET /humidity
+  webServer.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(humidity, 3));
+  });
+
+  // HTTP GET /relay
+  webServer.on("/relay", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", digitalRead(RELAY_PIN) == HIGH ? "true" : "false");
+  });
+
+  // HTTP GET /humidityLowerBound
+  webServer.on("/humidityLowerBound", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(settings.humidityLowerBound, 3));
+  });
+
+  // HTTP GET /humidityUpperBound
+  webServer.on("/humidityUpperBound", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", String(settings.humidityUpperBound, 3));
+  });
+
+  // HTTP POST /humidityLowerBound
+  webServer.on("/humidityLowerBound", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if(request->hasParam("value", true)) {
+      AsyncWebParameter* value = request->getParam("value", true);
+      settings.humidityLowerBound = value->value().toFloat();
+      EEPROM.put(0, settings);
+      EEPROM.commitReset();
+    }
+    request->send(200, "text/plain", String(settings.humidityLowerBound, 3));
+  });
+
+  // HTTP POST /humidityUpperBound
+  webServer.on("/humidityUpperBound", HTTP_POST, [](AsyncWebServerRequest *request) {
+    if(request->hasParam("value", true)) {
+      AsyncWebParameter* value = request->getParam("value", true);
+      settings.humidityUpperBound = value->value().toFloat();
+      EEPROM.put(0, settings);
+      EEPROM.commitReset();
+    }
+    request->send(200, "text/plain", String(settings.humidityUpperBound, 3));
+  });
+
+  // Web Server.
+  webServer.begin();
+
+  // Arduino OTA.
+  ArduinoOTA.setPassword(OTA_PASSWORD);
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  
 }
 
 void loop() {
@@ -56,13 +158,12 @@ void loop() {
     lastSampled = millis();
     Serial.print("Humidity: ");
     Serial.println(String(humidity, 3));
-    if (humidity > RELAY_UPPER_BOUND) {
-      Serial.println("Turning off relay...");
+    if (humidity > settings.humidityUpperBound) {
       digitalWrite(RELAY_PIN, LOW);
     }
-    else if (humidity < RELAY_LOWER_BOUND) {
-      Serial.println("Turning on relay...");
+    else if (humidity < settings.humidityLowerBound) {
       digitalWrite(RELAY_PIN, HIGH);
     }
   }
+  ArduinoOTA.handle();
 }
